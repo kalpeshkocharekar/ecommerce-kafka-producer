b9aa4f6be1bed946b460e4d98ac311ca6841c7ac
Kalpesh Kocharekar <kalpeshk71@gmail.com>
2020-09-02 16:31:19 +0530

    added logic for random uuid, customer_id, product_id and quantity

--- src/main/scala/com/ecommerce/producer/recordproducer.scala
+++ src/main/scala/com/ecommerce/producer/recordproducer.scala
@@ -1,7 +1,7 @@
 package com.ecommerce.producer
 
 import java.util.Properties
-
+import java.util.UUID.randomUUID
 import com.typesafe.scalalogging.Logger
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import org.slf4j.LoggerFactory
@@ -25,15 +25,24 @@ object recordproducer {
       props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
       props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
       val producer = new KafkaProducer[String, String](props)
+      val start = 1
+      val end   = 20
+      val rnd = new scala.util.Random
+      val random_customer_id  = start + rnd.nextInt( (end - start) + 1 )
+      val random_product_id  = start + rnd.nextInt( (end - start) + 1 )
+      val start_quantity = 1
+      val end_quantity   = 5
+      val random_quantity  = start + rnd.nextInt( (end_quantity - start_quantity) + 1 )
+
       val jsonstring =
-        s"""{
-           | "order_id": "0001",
-           | "product_id": "Peter"
-           |  "customer_id": "Peter"
-           |   "quantity": "Peter"
+        f"""{
+           | "order_id": "${randomUUID().toString()}",
+           | "product_id": "${random_product_id}"
+           |  "customer_id": "${random_customer_id }"
+           |   "quantity": "${random_quantity}"
            |}
          """.stripMargin
-
+      print(jsonstring)
       val record = new ProducerRecord[String, String](topic, "testtable", jsonstring)
       producer.send(record)
       producer.close()
