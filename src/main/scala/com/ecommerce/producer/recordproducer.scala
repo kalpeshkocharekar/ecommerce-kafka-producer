997ab3677d3b52bf7f210c391d60b5549cf5cfb1
Kalpesh Kocharekar <kalpeshk71@gmail.com>
2020-09-06 17:09:07 +0530

    adding latest code

--- src/main/scala/com/ecommerce/producer/recordproducer.scala
+++ src/main/scala/com/ecommerce/producer/recordproducer.scala
@@ -13,8 +13,10 @@ object recordproducer {
   logger.info("starting job")
 
   def main(args: Array[String]): Unit = {
-
-    writeToKafka("producttopic")
+    while (true) {
+      writeToKafka("producttopic")
+      Thread.sleep(5000)
+    }
   }
 
 
@@ -26,22 +28,24 @@ object recordproducer {
       props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
       val producer = new KafkaProducer[String, String](props)
       val start = 1
-      val end   = 20
+      val end = 20
       val rnd = new scala.util.Random
-      val random_customer_id  = start + rnd.nextInt( (end - start) + 1 )
-      val random_product_id  = start + rnd.nextInt( (end - start) + 1 )
+      val random_customer_id = start + rnd.nextInt((end - start) + 1)
+      val random_product_id = start + rnd.nextInt((end - start) + 1)
       val start_quantity = 1
-      val end_quantity   = 5
-      val random_quantity  = start + rnd.nextInt( (end_quantity - start_quantity) + 1 )
+      val end_quantity = 5
+      val random_quantity = start + rnd.nextInt((end_quantity - start_quantity) + 1)
 
-      val jsonstring =
-        f"""{
-           | "order_id": "${randomUUID().toString()}",
-           | "product_id": "${random_product_id}"
-           |  "customer_id": "${random_customer_id }"
-           |   "quantity": "${random_quantity}"
-           |}
-         """.stripMargin
+            val jsonstring =
+              s"""{
+                 | "order_id": "${randomUUID().toString()}",
+                 | "product_id": "${random_product_id.toString()}",
+                 |  "customer_id": "${random_customer_id.toString() }",
+                 |   "quantity": "${random_quantity.toString()}"
+                 |}
+               """.stripMargin
+
+
       print(jsonstring)
       val record = new ProducerRecord[String, String](topic, "testtable", jsonstring)
       producer.send(record)
--- src/main/scala/com/ecommerce/producer/sparkstreaming.scala
+++ src/main/scala/com/ecommerce/producer/sparkstreaming.scala
@@ -4,7 +4,7 @@ import com.ecommerce.producer.sparkrecevier.{inputtopic, kafkabroker}
 import com.typesafe.scalalogging.Logger
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{col, from_json}
-import org.apache.spark.sql.types.{StringType, StructType}
+import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
 import org.slf4j.LoggerFactory
 
 import scala.util.Properties
@@ -14,16 +14,17 @@ object sparkstreaming {
   private val logger: Logger = Logger(LoggerFactory.getLogger(sparkstreaming.getClass.getCanonicalName))
   private final val sparkLogLevel = Properties.envOrElse("SPARK_LOG_LEVEL", "ERROR")
   private final val identityMapCapacity = 128
+
   def main(args: Array[String]): Unit = {
 
-      val spark = SparkSession.builder()
-        .appName("structure streaming")
-        .master("local[*]")
-        .config("spark.driver.extraJavaOptions", "-Dlog4j.configuration=file:conf/log4j.properties")
-        .getOrCreate()
+    val spark = SparkSession.builder()
+      .appName("structure streaming")
+      .master("local[*]")
+      .config("spark.driver.extraJavaOptions", "-Dlog4j.configuration=file:conf/log4j.properties")
+      .getOrCreate()
 
-      spark.sparkContext.setLogLevel("ERROR")
-
+    spark.sparkContext.setLogLevel("ERROR")
+    
 
     println("starting spark streaming job")
     val df = spark
@@ -34,18 +35,44 @@ object sparkstreaming {
       .load()
 
     val data_Schema: StructType = new StructType()
-      .add("id", StringType)
-      .add("name", StringType)
+      .add("order_id", StringType)
+      .add("product_id", StringType)
+      .add("customer_id", StringType)
+      .add("quantity", StringType)
+    //      val data_Schema: StructType = new StructType()
+    //        .add("order_id", StringType)
+    //        .add("product_id", StringType)
+    //        .add("customer_id",StringType)
+    //        .add("quantity",StringType)
 
-    val jsondf =  df.select(col("key").cast("String"),col("value").cast("String"))
-      .withColumn("value",from_json(col("value"),data_Schema))
-    val finaldf  = jsondf.select(col("key"),col("value.*"))
-    finaldf.writeStream
+
+    val jsondf = df.select(col("key").cast("String"), col("value"))
+      .withColumn("value", from_json(col("value").cast("String"), data_Schema))
+
+    val finaldf = jsondf.select(col("key"), col("value.*"))
+
+    val typecastdf = finaldf
+      .withColumn("product_id", col("product_id").cast("Integer"))
+      .withColumn("customer_id", col("customer_id").cast("Integer"))
+      .withColumn("quantity", col("quantity").cast("Integer"))
+
+    typecastdf.printSchema()
+//
+//    val customertabledf = spark.read
+//      .format("jdbc")
+//      .option("driver","org.sqlite.JDBC")
+//      .option("dbtable","testtable")
+//      .option("url","/home/kalpesh/IdeaProjects/ecommerce-kafka-producer/devdb.sqlite")
+//      .load()
+//
+//    val joineddf = customertabledf.join(typecastdf,col("id"))
+//
+    typecastdf.writeStream
       .format("console")
       .start()
       .awaitTermination()
 
-    }
+  }
 
 
 }
